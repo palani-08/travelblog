e95fd4fd38e0aa1f520ee16358cd5c7752f009c4
palani-08 <palani7pavan@gmail.com>
2021-04-30 01:40:30 +0530

    Initial commit

---
+++ html/vueapp/ecommerce/src/components/ImgCard.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <div>
+        <b-col>
+      <b-card 
+      title="This is a Card"
+      img-src="@/components/download (4).jpeg"
+      img-alt="Nike"
+      img-top
+      tag="article"
+      style="max-width:20rem"
+      class="mb-2"
+      >
+        <b-card-text>
+some text here some text here some text here some text here some text here some text here
+        </b-card-text>
+        <b-button variant="primary">View</b-button>
+      </b-card>
+    </b-col>
+
+    </div>
+
+</template>
+
+
+
+
+<script>
+
+
+export default {
+  name:"home",
+components: {
+    
+    
+}
+        
+    };
+
+</script>
+
+<style  lang="scss" scoped>
+
+</style>
---
+++ html/vueapp/ecommerce/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+ <b-container>
+  <b-row align-v="center">
+    <img-card></img-card>
+    <img-card></img-card>
+    <img-card></img-card>
+    <img-card></img-card>
+   
+   
+  </b-row>
+</b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import ImgCard from "@/components/ImgCard.vue";
+
+export default {
+  name: "home",
+  components: {"img-card": ImgCard}
+};
+</script>
