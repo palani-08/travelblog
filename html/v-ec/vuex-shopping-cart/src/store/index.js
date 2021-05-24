752f09354cd0bd85a29610c10fc9881ddcb966a2
palani-08 <palani7pavan@gmail.com>
2021-05-24 22:23:31 +0530

    New v-ec file

---
+++ html/v-ec/vuex-shopping-cart/src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+import product from'./modules/product';
+import cart from './modules/cart';
+export default createStore({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    product,
+    cart
+  }
+})
